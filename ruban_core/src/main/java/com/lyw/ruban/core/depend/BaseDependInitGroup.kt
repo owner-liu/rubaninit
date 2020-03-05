6707346a8ec0414439d889d2930b313586d5b2dc
lyw <598299225@qq.com>
2020-03-05 21:01:03 +0800

    feat(structure): 定义相关基础类

--- ruban_core/src/main/java/com/lyw/ruban/core/depend/BaseDependInitGroup.kt
+++ ruban_core/src/main/java/com/lyw/ruban/core/depend/BaseDependInitGroup.kt
@@ -1,6 +1,6 @@
 package com.lyw.ruban.core.depend
 
-import com.lyw.ruban.core.thread.BaseThreadInitGroup
+import com.lyw.ruban.core.BaseInitGroup
 
 /**
  * Created on  2020-03-05
@@ -10,9 +10,8 @@ import com.lyw.ruban.core.thread.BaseThreadInitGroup
 abstract class BaseDependInitGroup
 //记录相关的依赖别名～
 constructor(
-    var baseDependGroupAlias: ArrayList<String>,
-    var baseDependGroupThreadCode: Int
-) : BaseThreadInitGroup(baseDependGroupThreadCode)
+    var treeMapDependGroupAliases: ArrayList<String>
+) : BaseInitGroup<IDependInitObserver>()
     , IDepend {
 
     override fun getAlias(): String {
