82738af6ecc82ad5cb9f1efe865a29bb3b05f332
lyw <598299225@qq.com>
2020-03-09 09:14:39 +0800

    feat(structure): depend,thread

---
+++ ruban/src/main/java/com/lyw/ruban/init/widgets/DependThreadLibInit.kt
@@ -0,0 +1,21 @@
+package com.lyw.ruban.init.widgets
+
+import com.lyw.ruban.core.IInitObserver
+import com.lyw.ruban.core.depend.DependInitContainer
+import com.lyw.ruban.init.lib.LibInit
+
+/**
+ * Created on  2020-03-08
+ * Created by  lyw
+ * Created for depend lib init
+ */
+class DependThreadLibInit
+//constructor(
+//    aliasList: ArrayList<String>,
+//    var dependLibInit: ThreadLibInit
+//) : DependInitContainer<IInitObserver>(aliasList, dependLibInit) {
+//
+//    override fun getAliasName(): String {
+//        return dependLibInit.getAliasName()
+//    }
+//}
---
+++ ruban/src/main/java/com/lyw/ruban/init/widgets/ThreadLibInit.kt
@@ -0,0 +1,22 @@
+package com.lyw.ruban.init.widgets
+
+import com.lyw.ruban.core.IInitObserver
+import com.lyw.ruban.core.depend.DependInitContainer
+import com.lyw.ruban.core.thread.ThreadInitContainer
+import com.lyw.ruban.init.lib.LibInit
+
+/**
+ * Created on  2020-03-08
+ * Created by  lyw
+ * Created for thread lib init
+ */
+class ThreadLibInit
+constructor(
+    threadCode: Int,
+    var ThreadLibInit: LibInit
+) : ThreadInitContainer<IInitObserver>(threadCode, ThreadLibInit) {
+
+    override fun getAliasName(): String {
+        return ThreadLibInit.getAliasName()
+    }
+}
