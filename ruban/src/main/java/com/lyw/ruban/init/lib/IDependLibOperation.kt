048c9ae857ff9ef17204b1feb4dfb4ae4f69bdbf
lyw <598299225@qq.com>
2020-03-10 18:01:07 +0800

    feat(structure):optimize

--- ruban/src/main/java/com/lyw/ruban/init/lib/IDependLibOperation.kt
+++ ruban/src/main/java/com/lyw/ruban/init/lib/IDependLibOperation.kt
@@ -1,14 +1,10 @@
 package com.lyw.ruban.init.lib
 
-import com.lyw.ruban.init.widgets.DependLibInit
-
 /**
  * Created on  2020-03-09
  * Created by  lyw
  * Created for
  */
 interface IDependLibOperation : ILibOperation {
-    fun addDependLibInit(init: DependLibInit)
-
     fun addModuleDependAlias(moduleCode: Int, list: ArrayList<String>)
 }
