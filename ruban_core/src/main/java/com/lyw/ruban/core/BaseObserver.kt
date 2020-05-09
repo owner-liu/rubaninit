3af526cb94b17856bbcf263488aca184b5689685
lyw <598299225@qq.com>
2020-05-09 14:07:40 +0800

    feat(config): 线程异常

--- ruban_core/src/main/java/com/lyw/ruban/core/BaseObserver.kt
+++ ruban_core/src/main/java/com/lyw/ruban/core/BaseObserver.kt
@@ -1,5 +1,7 @@
 package com.lyw.ruban.core
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 /**
  * Created on  2020-03-08
  * Created by  lyw
@@ -9,5 +11,5 @@ open class BaseObserver {
 
     var initCount: Int = 0
 
-    var mInitCompletedAliases = arrayListOf<String>()
+    var mInitCompletedAliases = CopyOnWriteArrayList<String>()
 }
